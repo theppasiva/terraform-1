33b5b1faae7c6ec97d6ec432bfc4a2f9d0c05714
theppasiva <theppasiva@gmail.com>
2024-08-18 01:14:40 +0530

    terraform ec2

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -1,8 +1,65 @@
 resource "aws_instance" "web" {
   ami           = "ami-0b4f379183e5706b9" #devops-practice
   instance_type = "t2.micro"
+  #vpc_security_group_ids=[aws_security_group.roboshop-all.id]
+  vpc_security_group_ids=[aws_security_group.allow_tls.id]
 
   tags = {
     Name = "HelloTerraform"
   }
 }
+
+resource "aws_security_group" "roboshop-all" {
+  name        = "roboshop-all-aws"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  #vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "Allow all ports"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = ["::/0"]
+  }
+
+   tags = {
+   Name = "roboshop-all-aws"
+  }
+}
+ 
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  #vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  #cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 0
+  ip_protocol       = "tcp"
+  to_port           = 0
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+
+  
+
