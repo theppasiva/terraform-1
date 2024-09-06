ce370aa98b6a1c00b30c2a7832cb501319e85a70
theppasiva <theppasiva@gmail.com>
2024-09-06 16:57:09 +0530

    conditions

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
   ami           = "ami-0b4f379183e5706b9" #devops-practice
   instance_type = "t2.micro"
-  #vpc_security_group_ids=[aws_security_group.roboshop-all.id]
-  vpc_security_group_ids=[aws_security_group.allow_tls.id]
+  vpc_security_group_ids=[aws_security_group.roboshop-all.id]
+  #vpc_security_group_ids=[aws_security_group.allow_tls.id]
 
   tags = {
     Name = "HelloTerraform"
@@ -35,30 +35,30 @@ resource "aws_security_group" "roboshop-all" {
   }
 }
  
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  #vpc_id      = aws_vpc.main.id
+# resource "aws_security_group" "allow_tls" {
+#   name        = "allow_tls"
+#   description = "Allow TLS inbound traffic and all outbound traffic"
+#   #vpc_id      = aws_vpc.main.id
 
-  tags = {
-    Name = "allow_tls"
-  }
-}
+#   tags = {
+#     Name = "allow_tls"
+#   }
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  #cidr_ipv4         = aws_vpc.main.cidr_block
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 0
-  ip_protocol       = "tcp"
-  to_port           = 0
-}
+# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   #cidr_ipv4         = aws_vpc.main.cidr_block
+#   cidr_ipv4         = "0.0.0.0/0"
+#   from_port         = 0
+#   ip_protocol       = "tcp"
+#   to_port           = 0
+# }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
+# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "-1" # semantically equivalent to all ports
+# }
 
 
   
